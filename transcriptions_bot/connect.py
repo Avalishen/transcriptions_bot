a0189e306a65c28cc1e05ffda47b402ed3a9a1be
Avalishen <121102829+Avalishen@users.noreply.github.com>
2025-06-18 14:12:17 +0300

    connect.py

--- transcriptions_bot/connect.py
+++ transcriptions_bot/connect.py
@@ -1,7 +1,7 @@
 import whisper
 
-TOKEN = "7704364728:AAF2kaefczrvMtwbK2sC9PvEKGFkIt8yV3Q"
-MODEL_PATH = r"C:\Users\denis\PycharmProjects\transcriptions_bot\model"
-FFMPEG_PATH = r"C:\ffmpeg-7.1.1\tools\ffmpeg\bin\ffmpeg.exe"
-GENIUS_API_TOKEN = "4wQygJZ_0sfgtukAWZTUDYbSS0991sTX0ldsFLXG_-mbPiG2YVoKjwsGZmcF0M_S"
+TOKEN = ""
+MODEL_PATH = r""
+FFMPEG_PATH = r""
+GENIUS_API_TOKEN = ""
 model = whisper.load_model("large")
